aff9f859e2161a69f8241bd7b27c1acefe3647ec
Tudor Timi <tudortimi@users.noreply.github.com>
2020-05-01 13:06:37 +0200

    Add formal prop skeleton and OneSpin setup

---
+++ run/onespin/init.tcl
@@ -0,0 +1,9 @@
+set root $::env(ROOT)
+
+set_mode setup
+read_verilog -sv $root/src/dut/sv/shape_processor.sv
+elaborate
+compile
+
+set_mode mv
+read_sva $root/src/formal/sv/shape_processor_props.sv
