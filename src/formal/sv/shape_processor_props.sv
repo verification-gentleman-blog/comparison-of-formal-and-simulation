a859cd779e992013e4a2b791ad19f3fb0bdb109d
Tudor Timi <tudortimi@users.noreply.github.com>
2020-05-01 17:38:34 +0200

    Enforce legal CTRL.SHAPE/OPERATION pairs in DUT register

--- src/formal/sv/shape_processor_props.sv
+++ src/formal/sv/shape_processor_props.sv
@@ -41,6 +41,9 @@ module shape_processor_props(
   only_legal_operations_in_ctrl_sfr: assert property (
       is_legal_operation(shape_processor.ctrl_sfr.operation));
 
+  only_legal_combinations_in_ctrl_sfr: assert property (
+      is_legal_combination(shape_processor.ctrl_sfr.shape, shape_processor.ctrl_sfr.operation));
+
 
   ctrl_sfr_reg write_data_as_ctrl_sfr;
   assign write_data_as_ctrl_sfr = write_data;
